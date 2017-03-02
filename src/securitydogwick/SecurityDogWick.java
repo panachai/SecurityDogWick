2b7715a94b243b1083c15dfc4ae3ff5db4443908
panachai <panachai.ny@gmail.com>
2017-03-02 20:52:56 +0700

    ui finish

--- src/securitydogwick/SecurityDogWick.java
+++ src/securitydogwick/SecurityDogWick.java
@@ -11,11 +11,13 @@ public class SecurityDogWick extends javax.swing.JFrame {
         public void run() {
 //Sleep -> Interrupted ปกติ
             int i = 0;
+            int hour = 0;
             while (!stop) {
                 try {
-                    lblShowClock.setText("i = " + i);
+                    lbShowClock.setText(String.format("%02d", hour) +":" + String.format("%02d", i));
                     i++;
                     Thread.sleep(50);
+                    
                 } catch (InterruptedException ie) {
                     i = 0;
                 }
@@ -34,8 +36,17 @@ public class SecurityDogWick extends javax.swing.JFrame {
 
         btStart = new javax.swing.JButton();
         btStop = new javax.swing.JButton();
-        lblShowClock = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        lbShowClock = new javax.swing.JLabel();
+        lbHeader = new javax.swing.JLabel();
+        lbOwner = new javax.swing.JLabel();
+        lbStatus = new javax.swing.JLabel();
+        lbDog = new javax.swing.JLabel();
+        lbPolice = new javax.swing.JLabel();
+        lbThife = new javax.swing.JLabel();
+        lbOwnerS = new javax.swing.JLabel();
+        lbDogS = new javax.swing.JLabel();
+        lbPoliceS = new javax.swing.JLabel();
+        lbThifeS = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -55,40 +66,104 @@ public class SecurityDogWick extends javax.swing.JFrame {
             }
         });
 
-        lblShowClock.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        lblShowClock.setText("00:00");
+        lbShowClock.setFont(new java.awt.Font("Tahoma", 0, 40)); // NOI18N
+        lbShowClock.setText("00:00");
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        jLabel1.setText("Security Dog Wick");
+        lbHeader.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
+        lbHeader.setText("Security Dog Wick");
+
+        lbOwner.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbOwner.setText("OWNER");
+
+        lbStatus.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
+        lbStatus.setText("STATUS");
+
+        lbDog.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbDog.setText("DOG");
+
+        lbPolice.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbPolice.setText("POLICE");
+
+        lbThife.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbThife.setText("THIFE");
+
+        lbOwnerS.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbOwnerS.setText("______");
+
+        lbDogS.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbDogS.setText("______");
+
+        lbPoliceS.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbPoliceS.setText("______");
+
+        lbThifeS.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lbThifeS.setText("______");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(84, 84, 84)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbOwner)
+                    .addComponent(lbDog)
+                    .addComponent(lbPolice)
+                    .addComponent(lbThife))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbOwnerS)
+                    .addComponent(lbDogS)
+                    .addComponent(lbPoliceS)
+                    .addComponent(lbThifeS))
+                .addGap(96, 96, 96))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(lbHeader))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(179, 179, 179)
+                        .addComponent(lbShowClock))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(151, 151, 151)
+                        .addComponent(lbStatus)))
+                .addContainerGap(51, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(btStart)
                 .addGap(73, 73, 73)
                 .addComponent(btStop)
                 .addGap(111, 111, 111))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(47, 47, 47)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(171, 171, 171)
-                        .addComponent(lblShowClock)))
-                .addContainerGap(51, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(lbHeader)
                 .addGap(18, 18, 18)
-                .addComponent(lblShowClock)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 150, Short.MAX_VALUE)
+                .addComponent(lbShowClock)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lbStatus)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lbOwner)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbDog)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbPolice)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbThife))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lbOwnerS)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbDogS)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbPoliceS)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbThifeS)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 98, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btStop)
                     .addComponent(btStart))
@@ -146,7 +221,16 @@ public class SecurityDogWick extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btStart;
     private javax.swing.JButton btStop;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel lblShowClock;
+    private javax.swing.JLabel lbDog;
+    private javax.swing.JLabel lbDogS;
+    private javax.swing.JLabel lbHeader;
+    private javax.swing.JLabel lbOwner;
+    private javax.swing.JLabel lbOwnerS;
+    private javax.swing.JLabel lbPolice;
+    private javax.swing.JLabel lbPoliceS;
+    private javax.swing.JLabel lbShowClock;
+    private javax.swing.JLabel lbStatus;
+    private javax.swing.JLabel lbThife;
+    private javax.swing.JLabel lbThifeS;
     // End of variables declaration//GEN-END:variables
 }
